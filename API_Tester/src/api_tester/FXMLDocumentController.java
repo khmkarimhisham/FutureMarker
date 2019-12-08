e7e6856ceeeeeb96ea0de88873ec894e088678a2
Karim Hisham <khm.karimhisham@gmail.com>
2019-12-08 23:58:45 +0200

    create API Tester

---
+++ API_Tester/src/api_tester/FXMLDocumentController.java
@@ -0,0 +1,240 @@
+package api_tester;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Label;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import net.lingala.zip4j.core.ZipFile;
+import net.lingala.zip4j.exception.ZipException;
+
+public class FXMLDocumentController implements Initializable {
+
+    FileChooser fileChooser = new FileChooser();
+    File file = null;
+    Alert a = new Alert(AlertType.INFORMATION);
+
+    @FXML
+    Label filePathLabel = new Label();
+
+    @FXML
+    private void handleChooserbtn(ActionEvent event) {
+        // configureFileChooser(fileChooser);
+        File tempFile = fileChooser.showOpenDialog(new Stage());
+        if (tempFile != null) {
+            this.file = tempFile;
+            filePathLabel.setText(this.file.getPath());
+        }
+    }
+
+    @FXML
+    private void handleOkbtn(ActionEvent event) throws IOException {
+        if (file != null) {
+            String ext = file.getName().substring(file.getName().lastIndexOf("."));
+            if (ext.equals(".java")) {
+                String response = sendFileToHTTP(file.getPath());
+                createResponseFile(file.getParent() + "/feedback.txt", response);
+            } else if (ext.equals(".zip")) {
+                handleZipFile();
+            }
+        } else {
+            a.setContentText("Please choose a file");
+            a.show();
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        List<String> myExtentions = new ArrayList<>();
+        myExtentions.add("*.java");
+        myExtentions.add("*.zip");
+        fileChooser.setTitle("Choose File");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("java or zip", myExtentions));
+        fileChooser.setInitialDirectory(
+                new File(System.getProperty("user.home"))
+        );
+    }
+
+    private String sendFileToHTTP(String filepath) {
+        String urlTo = "http://localhost/API/assess.php";
+
+        String filefield = "codeFile";
+
+        HttpURLConnection connection = null;
+        DataOutputStream outputStream = null;
+        InputStream inputStream = null;
+
+        String twoHyphens = "--";
+        String boundary = "*****" + Long.toString(System.currentTimeMillis()) + "*****";
+        String lineEnd = "\r\n";
+
+        String result = "";
+
+        int bytesRead, bytesAvailable, bufferSize;
+        byte[] buffer;
+        int maxBufferSize = 1 * 1024 * 1024;
+
+        try {
+            File filee = new File(filepath);
+            FileInputStream fileInputStream = new FileInputStream(filee);
+
+            URL url = new URL(urlTo);
+            connection = (HttpURLConnection) url.openConnection();
+
+            connection.setDoInput(true);
+            connection.setDoOutput(true);
+            connection.setUseCaches(false);
+
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Connection", "Keep-Alive");
+            connection.setRequestProperty("User-Agent", "Android Multipart HTTP Client 1.0");
+            connection.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
+
+            outputStream = new DataOutputStream(connection.getOutputStream());
+            outputStream.writeBytes(twoHyphens + boundary + lineEnd);
+            outputStream.writeBytes("Content-Disposition: form-data; name=\"" + filefield + "\"; filename=\"" + filepath + "\"" + lineEnd);
+
+            outputStream.writeBytes(lineEnd);
+
+            bytesAvailable = fileInputStream.available();
+            bufferSize = Math.min(bytesAvailable, maxBufferSize);
+            buffer = new byte[bufferSize];
+
+            bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+            while (bytesRead > 0) {
+                outputStream.write(buffer, 0, bufferSize);
+                bytesAvailable = fileInputStream.available();
+                bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+            }
+
+            outputStream.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+
+            inputStream = connection.getInputStream();
+            result = convertStreamToString(inputStream);
+
+            fileInputStream.close();
+            inputStream.close();
+            outputStream.flush();
+            outputStream.close();
+
+            return result;
+
+        } catch (Exception e) {
+            return e.toString();
+        }
+    }
+
+    private static String convertStreamToString(InputStream is) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+
+        String line = null;
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return sb.toString();
+    }
+
+    public void handleZipFile() throws IOException {
+        String destination = "tempFolder";
+        try {
+            ZipFile zipFile = new ZipFile(file.getAbsolutePath());
+            zipFile.extractAll(destination);
+
+            // File object 
+            File maindir = new File(destination);
+
+            if (maindir.exists() && maindir.isDirectory()) {
+                // array for files and sub-directories  
+                // of directory pointed by maindir 
+                File arr[] = maindir.listFiles();
+
+                System.out.println("**********************************************");
+                System.out.println("Files from main directory : " + maindir);
+                System.out.println("**********************************************");
+
+                // Calling recursive method 
+                exploreFiles(arr, 0, 0);
+            }
+        } catch (ZipException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void exploreFiles(File[] arr, int index, int level) throws IOException {
+        // terminate condition 
+        if (index >= arr.length) {
+            return;
+        }
+
+        // for files 
+        if (arr[index].isFile()) {
+            String tempFilePath = arr[index].getPath();
+            String response = sendFileToHTTP(tempFilePath);
+            createResponseFile(arr[index].getParent() + "/feedback.txt", response);
+            // for sub-directories 
+        } else if (arr[index].isDirectory()) {
+            int count = arr[index].listFiles().length;
+            // recursion for sub-directories 
+            File[] files = arr[index].listFiles();
+            if (count <= 1) {
+                exploreFiles(files, 0, level + 1);
+
+            } else {
+                File lastFile = files[0];
+                for (int i = 1; i < files.length; i++) {
+                    if (lastFile.getName().contains(" " + i)) {
+                        lastFile = files[i];
+                    }
+                }
+                exploreFiles(lastFile.listFiles(), 0, level + 2);
+            }
+        }
+        // recursion for main directory 
+        exploreFiles(arr, ++index, level);
+    }
+
+    public void createResponseFile(String filePath, String response) throws IOException {
+        File file = new File(filePath);
+
+        //Create the file
+        if (file.createNewFile()) {
+            System.out.println("File is created!");
+        } else {
+            System.out.println("File already exists.");
+        }
+        //Write Content
+        FileWriter writer = new FileWriter(file);
+        writer.write(response);
+        writer.flush();
+        writer.close();
+    }
+}
