50a65654ea282b70451ce9419022205638df3547
haithamhashem <45846334+haithamhashem@users.noreply.github.com>
2019-12-17 23:24:10 +0200

    run file change

--- API/indentation_api/src/indentation_api/run.java
+++ API/indentation_api/src/indentation_api/run.java
@@ -15,9 +15,13 @@ public class run {
 
         String indentationOutput = "";
         String identifiersOutput = "";
+        String LineLengthOutput = "";
+        String MethodLengthOutput = "";
 
         double indentationCount = 0;
         double idenftifersCount = 0;
+        double LineLengthCount = 0;
+        double MethodLengthCount = 0;
 
         try {
             Process process = Runtime.getRuntime().exec(
@@ -34,18 +38,31 @@ public class run {
                 } else if (line.contains("[AbbreviationAsWordInName]")) {
                     identifiersOutput += line.substring(line.lastIndexOf("java:") + 5);
                     idenftifersCount += 1;
+                }else if (line.contains("[LineLength]")) {
+                    LineLengthOutput += line.substring(line.lastIndexOf("java:") + 5);
+                    LineLengthCount += 1;
+                }else if (line.contains("[MethodLength]")) {
+                    MethodLengthOutput += line.substring(line.lastIndexOf("java:") + 5);
+                    MethodLengthCount += 1;
                 }
             }
             result.put("Indentation output", indentationOutput);
             result.put("Identifiers output", identifiersOutput);
+            result.put("LineLength output", LineLengthOutput);
+            result.put("MethodLength output", MethodLengthOutput);
+            
         } catch (IOException e) {
             throw e;
         }
 
         double indentationGrade = 1.0 - (indentationCount / codeLineCount);
         double identifiersGrade = 1.0 - (idenftifersCount / codeLineCount);
+        double LineLengthGrade = 1.0 - (LineLengthCount / codeLineCount);
+        double MethodLengthGrade = 1.0 - (MethodLengthCount / codeLineCount);
         result.put("Indentation Grade", indentationGrade);
-        result.put("Identifiers Grade", identifiersGrade);
+        result.put("ineLength Grade", LineLengthGrade);
+        result.put("MethodLength Grade", MethodLengthGrade);
+        
         System.out.println(result);
     }
 
