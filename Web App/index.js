3176f88563eba103f2de604b410e0c60df26a538
AnasHassanGouda <33873054+AnasHassanGouda@users.noreply.github.com>
2020-02-08 11:59:08 +0200

    course page+lec page +home page +some edits in index page+profile page

--- Web App/index.js
+++ Web App/index.js
@@ -6,3 +6,12 @@ $(window).on("scroll", function() {
        $(".header").removeClass("active");
     }
 });
+$(document).ready(function() {
+  // SideNav Button Initialization
+  $(".button-collapse").sideNav({
+    slim: true
+  });
+  // SideNav Scrollbar Initialization
+  var sideNavScrollbar = document.querySelector('.custom-scrollbar');
+  var ps = new PerfectScrollbar(sideNavScrollbar);
+})
