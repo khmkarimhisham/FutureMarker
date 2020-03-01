73ec81ecca55d93f358f4e07cf7a8b211efe5001
Karim Hisham <khm.karimhisham@gmail.com>
2020-03-01 23:41:52 +0200

    Update assess.php

--- WebApp/API/assess.php
+++ WebApp/API/assess.php
@@ -1,6 +1,30 @@
 <?php
 
 require '../DB/db.php';
+function indentation_msg($x)
+{
+    if (preg_match_all("/[1-9]*:/", $x, $matches)) {
+        $output = "";
+        for ($z = 0; $z < count($matches[0]); $z++) {
+            $output .= "Line " . str_replace(":", "", $matches[0][$z]) . " has incorrect indentation<br>";
+        }
+        return ($output);
+    } else {
+        return "Your code is very clear";
+    }
+}
+function identifiers_msg($x)
+{
+    if (preg_match_all("/[1-9]*:/", $x, $matches)) {
+        $output = "";
+        for ($z = 0; $z < count($matches[0]); $z++) {
+            $output .= "The identifier name should be more obvious at line " . str_replace(":", "", $matches[0][$z]) . "<br>";
+        }
+        return ($output);
+    } else {
+        return "Your identifiers name are clear";
+    }
+}
 
 if (isset($_FILES['codeFile'])) {
     $error_message = "";
@@ -57,12 +81,12 @@ if (isset($_FILES['codeFile'])) {
 
             $sql = "SELECT * FROM `dynamic_test` WHERE `Assignment_ID` = '$Assignment_ID'";
             $result = mysqli_query($db_connection, $sql);
-
+            $done_dynamic_test_feedback = "";
+            $done_feature_test_feedback = "";
             if (mysqli_num_rows($result) > 0) {
                 $dynamic_count = 0;
                 while ($row = mysqli_fetch_assoc($result)) {
                     exec("java -jar testcase_api/dist/testcase_api.jar \"" . $row['Input'] . "\" \"" . $row['output']  . "\" \"" . $file_full_path . "\"", $output4);
-
                     $temp_json = json_decode($output4[0]);
                     $tests[$dynamic_count] = $temp_json->{'TestCase'};
                     $dynamic_count++;
@@ -77,23 +101,25 @@ if (isset($_FILES['codeFile'])) {
 
             if ($response->{'build'}) {
                 $compilation_feedback = "The code compiled successfully";
-
                 $done_compilation_grade = $ass_dynamic_test_grade;
                 $done_dynamic_test_grade = (array_count_values($response->{'TestCase'})['true'] / $dynamic_count) * $ass_dynamic_test_grade;
             } else {
                 $compilation_feedback = "The code failed to compile";
             }
-
             if ($response->{'percentage'} < 0.1) {
                 $comment_feedback = "You need to clarify your code by using more comments";
             } else {
                 $comment_feedback = "Your comments made the code clear";
             }
-            $indentation_feedback = mysqli_real_escape_string($db_connection, $response->{'Indentation output'});
-            $Identifiers_feedback = mysqli_real_escape_string($db_connection, $response->{'Identifiers output'});
+
+            $indentation_feedback = indentation_msg($response->{'Indentation output'});
+
+            $Identifiers_feedback = identifiers_msg($response->{'Identifiers output'});
+
             $done_style_grade = (($response->{'Indentation Grade'} + $response->{'Identifiers Grade'} +  $response->{'percentage'}) / 3) * $ass_style_grade;
 
-            $sql = "INSERT INTO `doing_assignment`(`Student_ID`, `Assignment_ID`, `Assignment_dir`, `Assignment_main`, `Compilation_grade`, `Compilation_feedback`, `Style_grade`, `Comment_feedback`, `Indentation_feedback`, `Methods_feedback`, `Identifiers_feedback`, `Dynamic_test_grade`, `Feature_test_grade`) VALUES ('$Student_ID', '$Assignment_ID', '$file_full_path', '$file_full_path', '$done_compilation_grade', '$compilation_feedback', '$done_style_grade', '$comment_feedback', '$indentation_feedback', null, '$Identifiers_feedback', '$done_dynamic_test_grade', '$done_feature_test_grade')";
+
+            $sql = "INSERT INTO `doing_assignment`(`Student_ID`, `Assignment_ID`, `Assignment_dir`, `Assignment_main`, `Compilation_grade`, `Compilation_feedback`, `Style_grade`, `Comment_feedback`, `Indentation_feedback`, `Methods_feedback`, `Identifiers_feedback`, `Dynamic_test_grade`, `Dynamic_test_feedback`, `Feature_test_grade`, Feature_test_feedback) VALUES ('$Student_ID', '$Assignment_ID', '$file_full_path', '$file_full_path', '$done_compilation_grade', '$compilation_feedback', '$done_style_grade', '$comment_feedback', '$indentation_feedback', null, '$Identifiers_feedback', '$done_dynamic_test_grade', '$done_dynamic_test_feedback', '$done_feature_test_grade', '$done_feature_test_feedback')";
             $result = mysqli_query($db_connection, $sql);
             if ($result) {
                 header("Location: ../Assignment_body.php?course_id=$Course_ID&assignment_id=$Assignment_ID&submit=true");
