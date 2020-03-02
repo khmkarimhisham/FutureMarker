43df0276ebf03c9f588d0071dcac96951488be32
AnasHassanGouda <33873054+AnasHassanGouda@users.noreply.github.com>
2020-03-03 00:29:50 +0200

    Complete Profile page without upload image to database^_^

--- WebApp/Profile.php
+++ WebApp/Profile.php
@@ -3,13 +3,14 @@
 session_start();
 require 'DB/db.php';
 $email = $_SESSION['User_email'];
-
+$usertype = $_SESSION['User_type'];
 $User_ID = $_SESSION['User_ID'];
 $error_message = "";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $bio = $_POST["bio"];
     $phone = $_POST["phone"];
+  //  $user_image=$POST[""];
     if ($_SESSION['User_type'] == "instructor") {
         $query = "UPDATE `instructor`SET `Instructor_bio`='$bio',`Instructor_phone`='$phone' WHERE  `Instructor_ID`= $User_ID";
         $query2 = mysqli_query($db_connection, $query);
@@ -38,7 +39,7 @@ if ($_SESSION['User_type'] == "instructor") {
     $sql = "SELECT * FROM `student` WHERE `Student_ID`= $User_ID";
     $res = mysqli_query($db_connection, $sql);
     $row = mysqli_fetch_assoc($res);
-    print_r($row);
+    //print_r($row);
     if ($res) {
         $bio = $row['Student_bio'];
         $Phone = $row['Student_phone'];
@@ -79,8 +80,8 @@ if ($_SESSION['User_type'] == "instructor") {
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="coures.html">
+            <li class="nav-item active">
+                    <a class="nav-link" href="<?php echo $usertype == "instructor" ?  "courses_instructor.php" : "courses_student.php"; ?>">
                         Courses
                         <span class="sr-only">(current)</span>
                     </a>
@@ -100,7 +101,7 @@ if ($_SESSION['User_type'] == "instructor") {
 
 
             </ul>
-            <ul class="navbar-nav ">
+            <ul class="navbar-nav navedit ">
                 <li class="nav-item">
                     <a class="nav-link" href="#">
                         <i class="fa fa-bell">
@@ -126,14 +127,16 @@ if ($_SESSION['User_type'] == "instructor") {
                 <li>
                     <div class="dropdown mydrop">
                         <button type="button" class="btn btn-primary dropdown-toggle mydropbutton" data-toggle="dropdown">
-                            <img src="http://www.bobmazzo.com/wp-content/uploads/2009/07/bobmazzoCD.jpg" width="30" height="30">
-                            <?php echo $email; ?>
+                            <img src="<?php echo $_SESSION['User_image']; ?>" width="30" height="30">
+
+                            <?php echo $_SESSION['User_email'];
+                            ?>
                         </button>
                         <div class="dropdown-menu">
-                            <a class="dropdown-item" href="Profile.html">Your Profile</a>
+                            <a class="dropdown-item" href="Profile.php">Your Profile</a>
                             <a class="dropdown-item" href="#">Future Academy</a>
                             <a class="dropdown-item" href="#">Settings</a>
-                            <a class="dropdown-item" href="#"><i class="fa fa-sign-out"></i>Log out</a>
+                            <a class="dropdown-item" href="logout.php"><i class="fa fa-sign-out"></i>Log out</a>
                         </div>
                     </div>
                 </li>
@@ -143,100 +146,124 @@ if ($_SESSION['User_type'] == "instructor") {
 
         </div>
     </nav>
+    <div class="container">
+        <div class="row">
 
-    <div class="row">
+            <div class="col-4">
+                <div class="card -row my-5"style=" border-radius: 25px;">
+                    <div class="card-body">
 
-        <div class="col-6 col-md-4">
-            <div class="card -row my-5">
-                <div class="card-body">
+                        <div class="text-center">
+                            <img src="images/avatar2.jpg" width="240" height="240" class="avatar img-circle img-thumbnail" alt="avatar">
+                            <h6>Upload a different photo...</h6>
+                            <input type="file" class="text-center center-block file-upload" style="margin-left: 40px;">
+                            <script>
+                                $(document).ready(function() {
 
-                    <div class="container">
-                        <img src="images/avatar.jpg" class="rounded mx-auto d-block" alt="..." width="250" height="150">
 
-                    </div>
-                    <hr class="my-1">
-                    <label class="profilelebal">my Courses</label>
-                    <hr class="my-1">
-                    <?php
+                                    var readURL = function(input) {
+                                        if (input.files && input.files[0]) {
+                                            var reader = new FileReader();
 
-                    $query = mysqli_query($db_connection, "SELECT course.Course_ID, course.Course_name, course.Course_image FROM `Course` JOIN `teaches` ON teaches.Course_ID = course.Course_ID WHERE teaches.Instructor_ID = $User_ID");
-                    if (mysqli_num_rows($query) > 0) {
-                        while ($row = mysqli_fetch_assoc($query)) {
+                                            reader.onload = function(e) {
+                                                $('.avatar').attr('src', e.target.result);
+                                            }
 
-                            echo '
+                                            reader.readAsDataURL(input.files[0]);
+                                        }
+                                    }
+
+
+                                    $(".file-upload").on('change', function() {
+                                        readURL(this);
+                                    });
+                                });
+                            </script>
+                        </div>
+                        </hr><br>
+                        <hr class="my-1">
+                        <label class="profilelebal">My Courses</label>
+                        <hr class="my-1">
+                        <?php
+
+                        $query = mysqli_query($db_connection, "SELECT course.Course_ID, course.Course_name, course.Course_image FROM `Course` JOIN `teaches` ON teaches.Course_ID = course.Course_ID WHERE teaches.Instructor_ID = $User_ID");
+                        if (mysqli_num_rows($query) > 0) {
+                            while ($row = mysqli_fetch_assoc($query)) {
+
+                                echo '
                                         <div>
                                             <img src="' . $row['Course_image'] . '" width="40" height="40">
                                             <a href="course_content.php?course_id=' . $row['Course_ID'] . '">' . $row['Course_name'] . '</a>
                                         </div>
                                         <hr class="my-1">';
+                            }
                         }
-                    }
-                    ?>
+                        ?>
 
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="col-12 col-md-6">
-            <div class="card -row my-5">
-                <div class="card-body">
-                    <div class="container text-right">
-                        <a href="#" data-toggle="modal" data-target="#editForm">
-                            <label class="profilelebal">Edit Profile</label>
-                        </a>
-                    </div>
+            <div class="col-7">
+                <div class="card -row my-5" style=" border-radius: 25px;">
+                    <div class="card-body">
+                        <div class="container text-right">
+                            <a href="#" data-toggle="modal" data-target="#editForm">
+                                <label class="profilelebal">Edit Profile</label>
+                            </a>
+                        </div>
 
-                    <div class="modal fade" id="editForm" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div class="modal-dialog modal-dialog-centered" role="document">
-                            <div class="modal-content">
-                                <div class="modal-header border-bottom-0">
-                                    <h5 class="modal-title" id="exampleModalLabel">Update Profile</h5>
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                        <span aria-hidden="true">&times;</span>
-                                    </button>
+                        <div class="modal fade" id="editForm" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-centered" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header border-bottom-0">
+                                        <h5 class="modal-title" id="exampleModalLabel">Update Profile</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <form method="post" enctype="multipart/form-data">
+                                        <div class="modal-body">
+                                            <div class="form-group">
+                                                <label for="bio">Bio</label>
+                                                <input type="text" class="form-control" name="bio" id="bio" placeholder="Enter Bio">
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="phone">Phone</label>
+                                                <input type="text" class="form-control" name="phone" id="phone" placeholder="Enter your phone number">
+                                            </div>
+
+                                        </div>
+                                        <div class="modal-footer border-top-0 d-flex justify-content-center">
+                                            <button type="submit" class="btn btn-success">Submit</button>
+                                        </div>
+                                    </form>
                                 </div>
-                                <form method="post" enctype="multipart/form-data">
-                                    <div class="modal-body">
-                                        <div class="form-group">
-                                            <label for="bio">Bio</label>
-                                            <input type="text" class="form-control" name="bio" id="bio" placeholder="Enter Bio">
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="phone">Phone</label>
-                                            <input type="text" class="form-control" name="phone" id="phone" placeholder="Enter your phone number">
-                                        </div>
-
-                                    </div>
-                                    <div class="modal-footer border-top-0 d-flex justify-content-center">
-                                        <button type="submit" class="btn btn-success">Submit</button>
-                                    </div>
-                                </form>
                             </div>
                         </div>
+
+                      
+
+                        <hr>
+                        <label class="profilelebal"style="font-size: 24">Personal Information</label>
+                        <hr class="my-1">
+                        <div><span  class="profilelebal">Name:</span><label style="margin-left: 10px;font-size: 18;"><?php echo $firstN . " " . $lastN; ?> </label></div>
+                        <hr class="my-2">
+                        <div><span class="profilelebal">Bio:</span><label style="margin-left: 10px;font-size: 18;"><?php echo $bio; ?></label></div>
+                        <hr class="my-4">
+                        <label class="profilelebal"style="font-size: 24">Contact Information</label>
+                        <hr class="my-1">
+                        <div><span class="profilelebal">Email:</span><a href="#"><label style="margin-left: 10px;font-size: 18;"><?php echo $email; ?></label></a></div>
+                        <hr class="my-2">
+                        <div><span class="profilelebal">Phone:</span><a href="#"><label style="margin-left: 10px;font-size: 18;"><?php echo $Phone; ?></label></a></div>
+
                     </div>
 
-                    <div> <label style="font-size: 18"><?php echo $firstN . " " . $lastN . $email; ?> </label></div>
-
-                    <hr>
-                    <label class="profilelebal">About Me</label>
-                    <hr class="my-1">
-                    <div><span>Name:</span><label class="profilelebal"><?php echo $firstN . " " . $lastN; ?> </label></div>
-                    <hr class="my-1">
-                    <div><span>Bio:</span><label class="profilelebal"><?php echo $bio; ?></label></div>
-                    <hr class="my-4">
-                    <label class="profilelebal">Contact Information</label>
-                    <hr class="my-1">
-                    <div><span>Email:</span><a href="#"><label class="profilelebal"><?php echo $email; ?></label></a></div>
-                    <hr class="my-1">
-                    <div><span>Phone:</span><a href="#"><label class="profilelebal"><?php echo $Phone; ?></label></a></div>
 
                 </div>
-
-
             </div>
         </div>
+
     </div>
-
-
 
 
 
