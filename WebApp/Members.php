96dc23e9d649f7ca29675a0da64a5b61577e7282
AnasHassanGouda <33873054+AnasHassanGouda@users.noreply.github.com>
2020-03-06 14:07:49 +0200

    Members of course page without connect to database

---
+++ WebApp/Members.php
@@ -0,0 +1,153 @@
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    <link rel="stylesheet" href="CSS/content.css">
+    <link rel="stylesheet" href="CSS/couresbody.css">
+    <link href="CSS/default.css" rel="stylesheet" type="text/css" media="screen" />
+
+    <!-- Makes the file tree(s) expand/collapsae dynamically -->
+    <script src="JS/php_file_tree.js" type="text/javascript"></script>
+
+    <script type="text/javascript" src="JS/index.js"></script>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+    <!------ Include the above in your HEAD tag ---------->
+</head>
+
+<body style="background-color: f0f0f0">
+    <nav class="navbar navbar-icon-top navbar-expand-lg navbar-dark homeheader">
+        <a class="navbar-brand" href="Home.html">
+            <img class="navbar-brand" src="images/logo.png">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="coures.html">
+                        Courses
+                        <span class="sr-only">(current)</span>
+                    </a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">
+
+                        Groups
+                    </a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link " href="#">
+
+                        Grades
+                    </a>
+                </li>
+
+
+            </ul>
+            <ul class="navbar-nav navedit">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        <i class="fa fa-bell">
+                            <span class="badge badge-info">11</span>
+                        </i>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        <i class="fa fa-search">
+                            <span class="badge badge-success"></span>
+                        </i>
+
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        <i class="fa fa-envelope">
+                            <span class="badge badge-info">2</span>
+                        </i>
+                    </a>
+                </li>
+                <li>
+                    <div class="dropdown mydrop">
+                        <button type="button" class="btn btn-primary dropdown-toggle mydropbutton" data-toggle="dropdown">
+                            <img src="http://www.bobmazzo.com/wp-content/uploads/2009/07/bobmazzoCD.jpg" width="30" height="30">
+
+                            anashassan299@outlook.com
+                        </button>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="Profile.html">Your Profile</a>
+                            <a class="dropdown-item" href="#">Future Academy</a>
+                            <a class="dropdown-item" href="#">Settings</a>
+                            <a class="dropdown-item" href="#"><i class="fa fa-sign-out"></i>Log out</a>
+                        </div>
+                    </div>
+                </li>
+
+            </ul>
+
+
+        </div>
+    </nav>
+    <div class="container">
+        <div class="row">
+            <div class="col-4">
+                <div class="card -row my-5" style=" border-radius: 25px;">
+                    <div class="card-body">
+                        <div class="text-center">
+                            <!--hena ya martin t7ot el image bta3t el course-->
+                            <img src="images/course.png" width="240" height="240" class="avatar img-circle img-thumbnail" alt="avatar">
+                        </div>
+                        </hr><br>
+                        <div>
+                            <hr class="my-2">
+                            <div> <a class="aedit active" href="#"> <label>Matiral</label></a></div>
+                            <hr class="my-3">
+                            <div> <a class="aedit" href="#"> <label>Update</label></a></div>
+                            <hr class="my-3">
+                            <div> <a class="aedit" href="#"> <label>Grades</label></a></div>
+                            <hr class="my-3">
+                            <div> <a class="aedit" href="#"> <label>Members</label></a></div>
+                            <hr class="my-3">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-7">
+                <div class="card -row my-5" style=" border-radius: 25px;">
+                    <div class="card-body">
+                         <!--hena ya martin t7ot el name bta3 el course-->
+                    <label class="profilelebal" style="font-size: 24">Mobile Programming</label><br>
+                    <label class="profilelebal" style="font-size: 18">Members</label>
+                    <hr class="my-1">
+                    <table class="table table-sm table-light ">
+                         <!--hena ya martin el tag eli hatkrr wtab3n hatdef el image bta3t eluser e esmo-->
+                                <tr class="table-active "><img src="http://www.bobmazzo.com/wp-content/uploads/2009/07/bobmazzoCD.jpg" width="30" height="30"><a href="#" class="aedit">Anas Hassan</a> </tr>
+                                <hr class="my-2 ">
+                                <tr class="table-active "><img src="http://www.bobmazzo.com/wp-content/uploads/2009/07/bobmazzoCD.jpg" width="30" height="30"><a href="#" class="aedit">Martin Fawzy</a> </tr>
+                                <hr class="my-2 ">
+                                <tr class="table-active "><img src="http://www.bobmazzo.com/wp-content/uploads/2009/07/bobmazzoCD.jpg" width="30" height="30"><a href="#" class="aedit">Karim Hisham</a> </tr>
+                                <hr class="my-2 ">
+                                <tr class="table-active "><img src="http://www.bobmazzo.com/wp-content/uploads/2009/07/bobmazzoCD.jpg" width="30" height="30"><a href="#" class="aedit">Mohamed Essam</a> </tr>
+                                <hr class="my-2 ">
+
+                            </table>
+
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</body>
+
+</html>
