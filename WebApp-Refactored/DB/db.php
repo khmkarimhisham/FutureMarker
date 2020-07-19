0ecdb3434be8e3b75d7714d2a7362da3be90f7b1
Karim Hisham <khm.karimhisham@gmail.com>
2020-07-19 22:41:39 +0200

    delete the old webapp

--- WebApp-Refactored/DB/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-$db_connection = mysqli_connect("localhost","root","","futuremarker");
-// Check connection
-if (mysqli_connect_errno()){
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
-?>
